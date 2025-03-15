185a4f589ad32ed4a0912e0364a428a1156b8aa2
alejandro.ruiz.paton <alejandro.ruiz.paton@estudiantat.upc.edu>
2025-03-15 18:33:13 +0100

    Funciones útiles + generador de un posible estado inicial

--- Practica-1/code/src/clasesAIMA/State.java
+++ Practica-1/code/src/clasesAIMA/State.java
@@ -1,8 +1,12 @@
 package clasesAIMA;
+import IA.Red.Centro;
+import IA.Red.Sensor;
 import IA.Red.Sensores;
 import IA.Red.CentrosDatos;
 
 import java.util.*;
+
+import static java.lang.Math.sqrt;
 
 /**
  * Clase State que representa un DAG de conexiones
@@ -15,7 +19,7 @@ public class State {
     /**
      * Configura es escenario de la ejecución
      */
-    public class setStatic(Sensores s, CentrosDatos c) {
+    public static void setStatic(Sensores s, CentrosDatos c) {
         sensores = s;
         centros = c;
     }
@@ -32,6 +36,9 @@ public class State {
         // Volumen de datos transmitido por el sensor (máximo 3 * volumen de datos que él envía)
         public double volumen;
 
+        /**
+         * Antes de llamarse debe haberse configurado el escenario con setStatic
+         */
         public ConexionSensor(int i) {
             entrantes = new ArrayList<>();
             destino = Integer.MAX_VALUE;    // Sin asignar
@@ -52,51 +59,91 @@ public class State {
         int n = sensores.size();
         int m = centros.size();
         conexiones = new ConexionSensor[n];
-        contadorInputCentros = new int[m];  // Se inicializa a 0
+        // El índice 0 en centros corresponde al índice 1 en contadorInputCentros (necesario para diferenciar el índice 0 de los sensores y de los centros
+        contadorInputCentros = new int[m+1];  // Se inicializa a 0
         for (int i = 0; i < n; i++) {
             conexiones[i] = new ConexionSensor(i);
         }
     }
 
-    /* ------------------ CREADORAS DE ESTADOS INICIALES -------------------- */
+    /* ---------------------------- ÚTILES ---------------------------------- */
+
+    /**
+     * Accede al centro a través de su índice negativo
+     * @param i identificador negativo del centro
+     * @return Centro con identificador i
+     */
+    private Centro getCentro(int i) {
+        return centros.get(-i - 1);
+    }
+
+    /**
+     * Incrementa en 1 el número de conexiones que tiene el centro con índice i
+     * @param i identificador negativo del centro
+     */
+    private void incrementaContadorCentros(int i) {
+        contadorInputCentros[-i - 1]++;
+    }
 
 
+    /**
+     * Calcula la distancia entre i y j
+     * @param i apunta a j
+     * @param j es apuntado por j
+     * @return Distancia euclídea entre i y j
+     */
+    private double calcularDistancia(int i, int j) {
+        int xi, yi, xj, yj; // Coordenadas
 
-    /* --------------------------- OPERADORES ------------------------------- */
+        Sensor s1 = sensores.get(i);
+        xi = s1.getCoordX();
+        yi = s1.getCoordY();
 
+        if (j < 0) {    // Si j es un Centro
+            Centro c = getCentro(j);
+            xj = c.getCoordX();
+            yj = c.getCoordY();
+        }
+        else {          // Si j es un Sensor
+            Sensor s2 = sensores.get(j);
+            xj = s2.getCoordX();
+            yj = s2.getCoordY();
+        }
 
+        return Math.sqrt((xi - xj) * (xi - xj) + (yi - yj) * (yi - yj));
+    }
 
-    /* --------------------------- HEURÍSTICAS ------------------------------ */
-
-
-
-    /* --------------------------- OPERADORES ------------------------------- */
-
-
-
-    /* ----------------- COMPROBACIÓN DE RESTRICCIONES ---------------------- */
 
     /**
      * Algoritmo DFS que devuelve si desde el nodo inicio se puede encontrar algún ciclo
      */
-    private boolean dfs(int inicio, boolean[] visitado) {
+    private boolean dfs(int inicio, boolean[] visitadoGeneral) {
         Stack<Integer> stack = new Stack<>();
         stack.push(inicio);
+        boolean[] visitadoActual = new boolean[sensores.size()];
 
         while(!stack.isEmpty()) {
             int nodo = stack.pop();
 
-            if (!visitado[nodo]) {
-                visitado[nodo] = true;
-
-                // Añado en la pila el nodo al que apunta
-                stack.push(conexiones[nodo].destino);
-            }
-            else {  // Si ya lo había visitado entonces hay un ciclo
+            if (visitadoActual[nodo]) { // Si ya lo he visitado en esta iteración entonces he encontrado un ciclo
                 return true;
             }
+
+            if (!visitadoGeneral[nodo]) {   // Si no lo he visitado en alguna iteración entonces lo visito
+                visitadoGeneral[nodo] = true;
+                visitadoActual[nodo] = true;
+
+                // Añado en la pila el nodo al que apunta
+                int destino = conexiones[nodo].destino;
+                if (destino >= 0) {
+                    stack.push(destino);
+                }
+            }
+            else {  // Si ya lo he visitado en alguna iteración anterior significa que este camino ya lo he hecho y no había ciclos
+                return false;
+            }
         }
-        return false;   // No ha encontrado un ciclo en esta iteración
+        return false;
     }
 
 
@@ -106,11 +153,11 @@ public class State {
     public boolean encontrarCiclos() {
         int n = conexiones.length;
 
-        boolean visitado[] = new boolean[n];    // Se inicializa en false
+        boolean[] visitado = new boolean[n];    // Se inicializa en false
 
         for (int nodo = 0; nodo < n; nodo++) {
-            if (!visitado[nodo]) {
-                if (dfs(nodo, visitado)) return true;    // Ciclo encontrado
+            if (!visitado[nodo] && dfs(nodo, visitado)) {
+                return true;    // Ciclo encontrado
             }
         }
         return false;   // No hay ciclo en ninguna iteración
@@ -124,12 +171,88 @@ public class State {
         return conexiones[i].entrantes.size() < 3;
     }
 
+
     /**
-     *  Comprueba si al centro i se le puede añadir una conexión
+     *  Comprueba si al centro i se le puede añadir una conexión (i es un índice negativo)
      */
-    public boolean scentroApuntable(int i) {
-        return contadorInputCentros[i] < 25;
+    public boolean centroApuntable(int i) {
+        return contadorInputCentros[-i - 1] < 25;
     }
+
+
+    /* ------------------ CREADORAS DE ESTADOS INICIALES -------------------- */
+
+
+    /**
+     * Este generador de estado inicial funciona de la siguiente forma: (Objetivo minimizar distancias, no tiene en cuenta costes ni tampoco apura en conseguir las mínimas distancias siempre)
+     *      - Calcula las distancias de cada sensor al resto de centros.
+     *      - Cada sensor es mapeado al centro que se encuentre más cercano
+     *      - Asigna sensores a su centro sin llegar a sobrepasar el máximo de conexiones
+     *      - El resto de sensores son asignados a sensores que estén conectados al centro al que estaban mapeados
+     */
+    public void generadorGreedyMinDist() {
+        int n = sensores.size();
+        int m = centros.size();
+
+        // Mapeo de cada sensor al centro más cercano
+        int[] centroAsignado = new int[n];      // Guarda el centro más cercano
+        double[] minDistancia = new double[n];  // Guarda la distancia mínima a su centro
+        Arrays.fill(minDistancia, Double.MAX_VALUE);    // Asignamos distancia infinita al principio
+
+        for (int i = 0; i < n; i++) {   // Por cada sensor
+            for (int j = -1; j >= -m; j--) {   // Por cada centro    (Tienen ids negativas)
+                double dist = calcularDistancia(i, j);
+                if (dist < minDistancia[j]) {
+                    minDistancia[i] = dist;
+                    centroAsignado[i] = j;
+                }
+            }
+        }
+
+        // Asignación de sensores a su centro más cercano respetando el máximo de conexiones
+        List<Integer>[] sensoresPorCentro = new ArrayList[m];   // Esta estructura será útil para después poder conectar los sensores que se han quedado sin asignar a alguno que se haya conectado a su centro
+        for (int i = 0; i < m; i++) {
+            sensoresPorCentro[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int idCentro = centroAsignado[i];
+
+            if (centroApuntable(idCentro)) {
+                conexiones[i].destino = idCentro;
+                incrementaContadorCentros(idCentro);
+                sensoresPorCentro[idCentro].add(i);
+            }
+        }
+
+        // Reasignamos los sensores que no han podido conectarse a su centro
+        for (int i = 0; i < n; i++) {
+            if (conexiones[i].destino == Integer.MAX_VALUE) {
+                int idCentro = centroAsignado[i];
+
+                // Buscamos a qué sensor conectarlo
+                for (int sensor : sensoresPorCentro[idCentro]) {    // A la que encontramos uno al que nos podamos conectar lo hacemos y paramos (Es probable que se generen malas soluciones pero para empezar tenemos una aproximación
+                    if (sensorApuntable(sensor)) {
+                        conexiones[i].destino = sensor;
+                        conexiones[sensor].entrantes.add(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /* --------------------------- OPERADORES ------------------------------- */
+
+
+
+    /* --------------------------- HEURÍSTICAS ------------------------------ */
+
+
+
+    /* --------------------------- OPERADORES ------------------------------- */
+
+
 
     /* ------------------------------ OTROS --------------------------------- */
 
@@ -138,5 +261,23 @@ public class State {
      */
     public boolean is_goal() {
         return false;
+    }
+
+
+    // Para depurar
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Estado:\n");
+        for (int i = 0; i < conexiones.length; i++) {
+            sb.append("Sensor ").append(i).append(" → ");
+            if (conexiones[i].destino >= 0) {
+                sb.append("Sensor ").append(conexiones[i].destino);
+            } else {
+                sb.append("Centro ").append(-conexiones[i].destino - 1);
+            }
+            sb.append(" (").append(conexiones[i].volumen).append(" Mw)\n");
+        }
+        return sb.toString();
     }
 }
